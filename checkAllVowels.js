6467351fd22ca135c9551b69680993d1984514c2
Arief Rachman <arievrchman@gmail.com>
2018-11-27 20:24:58 +0700

    check all vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
+function checkAllVowels(input) {
 
+    const vowelWords = ['a', 'i', 'u', 'e', 'o'];
+    let countVowel = 0;
+
+    for (let i = 0; i < vowelWords.length; i++) {
+        for (let j = 0; j < input.length; j++) {
+            if (input[j].toLowerCase() === vowelWords[i]) {
+                countVowel++
+            }
+        }
+    }
+
+
+    return countVowel === vowelWords.length ? true : false
+
+}
+
+console.log(checkAllVowels('uang'));
+console.log(checkAllVowels('aIueO'));
