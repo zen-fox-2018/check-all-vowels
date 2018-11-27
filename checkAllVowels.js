d52d1c9468c5f1bfd6fb8cb2afb0b9df25578799
ShadowM8 <anton2014lenovo@gmail.com>
2018-11-27 11:55:28 +0700

    checkAllVowels.js done -anton

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,25 @@
+function checkAllVocal(string) {
+    var vocal = 'aiueo'
 
+    if (string.length === 0) {
+        return true
+    }
+    else {
+        var isVocal = false
+        for (var i = 0; i < vocal.length; i++) {
+            if (string[0] === vocal[i]) {
+                isVocal = true
+            }
+        }
+        if (isVocal === false) {
+            return false
+        }
+        else {
+            return checkAllVocal(string.slice(1))
+        }    
+    }
+}
+
+console.log(checkAllVocal('aiuooeo'))
+console.log(checkAllVocal('oreo'))
+console.log(checkAllVocal('auoooo'))
