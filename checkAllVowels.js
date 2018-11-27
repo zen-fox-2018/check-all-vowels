53df062105c651b0b3f491f894a533b09f22110b
Zhang <zhang@android-42520aa2424d51bc.localdomain>
2018-11-27 11:42:11 +0700

    done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,8 @@
+function checkVowel(input) {
+  let arrin = input.split('')
+  return arrin.every(x => x == 'a' || x =='e' || x == 'i' || x =='o' || x == 'u' )
+}
 
+console.log(checkVowel('aaa')); //true
+console.log(checkVowel('aba')); //false
+console.log(checkVowel('aiioa')) //true
