60b37b65712f1ceaa740a10fd48ea9a7cb056227
Mahdihrs <mahdiharis@outlook.com>
2018-11-27 21:04:13 +0700

    cek huruf vokal

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,15 @@
+function checkAllVowels(str) {
+  var vowels = 'aiueo'
 
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < vowels.length; j++) {
+      if (str[i] === vowels[j]) {
+        return false  
+      }
+    }
+  }
+  return true
+}
+
+console.log(checkAllVowels('approximately'))
+console.log(checkAllVowels('rhythm'))
