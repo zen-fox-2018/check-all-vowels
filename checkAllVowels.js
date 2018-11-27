8ae876a45ccf162cdd53278ef6b4f64656c764f1
fauzan_bintang <obin.aji@gmail.com>
2018-11-27 18:22:21 +0700

    done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,20 +1,27 @@
+// function checkVowel(word) {
+//     const vowels = ['a', 'i', 'u', 'e', 'o']
+//     for (let i = 0; i < word.length; i++) {
+//         let check = false
+//         vowels.forEach(e => {
+//             if (word[i] === e) {
+//                 check = true
+//             }
+//         });
+//         if (check === false) {
+//             return false
+//         }
+//     }
+//     return true
+// }
+
 function checkVowel(word) {
-    const vowels = ['a', 'i', 'u', 'e', 'o']
-    for (let i = 0; i < word.length; i++) {
-        let check = false
-        vowels.forEach(e => {
-            if (word[i] === e) {
-                check = true
-            }
-        });
-        if (check === false) {
-            return false
-        }
-    }
-    return true
+    word = word.split('')
+    return word.every(data => {
+        let kamus = ['a', 'i', 'u', 'e', 'o']
+        return kamus.includes(data)
+    })
 }
 
 console.log(checkVowel('aiu'));
 console.log(checkVowel('aiueo'));
 console.log(checkVowel('aikau'));
-
