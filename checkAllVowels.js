8f5e62a7ede3dd9413e31f6e49ee848559519b3a
maitreya <bluespider529@gmail.com>
2018-11-27 16:27:56 +0700

    done check all vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,16 @@
 
+function checkAllVowels(str) {
+    let strLowerCase = str.toLowerCase();
+    let vowels = "aiueo";
+    for(let i of strLowerCase) {
+        if(vowels.includes(i)) {
+            return true;
+        }
+    }
+    return false;
+}
+console.log(checkAllVowels("Saturn")); //true
+console.log(checkAllVowels("Mars")); //true
+console.log(checkAllVowels("gggg")); //false
+console.log(checkAllVowels("Sister")); //true
+console.log(checkAllVowels("Hymn")); //false
