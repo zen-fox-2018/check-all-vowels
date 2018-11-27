a5b664a42dead1aa087f3b332e037e8bbc75771d
kangman53 <hermanboy53@gmail.com>
2018-11-27 11:16:12 +0700

    check all vowels done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,19 @@
+function checkVowel(str) {
+  var vocal = 'aiueo';
+  for(var i = 0; i < str.length; i++){
+    var isCheck = false;
+    for(var j = 0; j < vocal.length; j++){
+      if(str[i] === vocal[j]){
+        isCheck = true;
+      }
+    }
+    if(!isCheck){
+      return false;
+    }
+  }
+  return true;
+}
 
+console.log(checkVowel('aiueeeiiiooo'));
+console.log(checkVowel('abaaaaaaaaaa'));
+console.log(checkVowel('aiiiii'));
