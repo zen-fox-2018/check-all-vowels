7d5a7ff0a424c0d9760a9a592150372e6449884d
ACER <fauziaazia171097@gmail.com>
2018-11-27 00:31:36 -0500

    finish with .every

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,10 @@
 
+function checkVowels(word) {
+   let kata = word.split('') 
+   return kata.every(alp => alp === "o" || alp === "i" || alp === "a" || alp === "u" || alp === "e") 
+}
+
+console.log(checkVowels("eejuii"))
+console.log(checkVowels("aiueo"))
+console.log(checkVowels("jjjjui"))
+console.log(checkVowels("iiiiii"))
