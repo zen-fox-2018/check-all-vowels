d644bb8b57b154d9417c033f48f723aa13dc669c
maria <msmariaulfa33@gmail.com>
2018-11-27 18:38:03 +0700

    submit

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,21 @@
 
+function checkvowel(str){
+    var kamus = ['a','i','u','e','o']
+    var vowel = ''
+    for(i=0; i<str.length; i++){
+        for(j=0; j<kamus.length; j++){
+            if(str[i].toLowerCase() === kamus[j]){
+                vowel+= kamus[j]
+            }
+        }
+    }
+    if(vowel.length === str.length){
+        return true
+    }else{
+        return false
+    }
+}
+
+console.log(checkvowel('apaya'))
+console.log(checkvowel('aiou'))
+
