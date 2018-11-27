01e78a58d7dc088147daba32741ee9966b47719e
Januarsmad <januarsmad@gmail.com>
2018-11-27 15:28:28 +0700

    done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,21 @@
+function checkVowel(word) {
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+    let arrWord = Array.from(word)
+    let temp = ''
 
+    arrWord.forEach((valueWord) => {
+        vowels.forEach((valueVowel) => {
+            if(valueWord === valueVowel) {
+                temp += valueWord
+            }
+        })
+    })
+
+    if(temp === word) {
+        return true
+    }
+    return false
+}
+
+console.log(checkVowel('programmer'))
+console.log(checkVowel('aeae'))
