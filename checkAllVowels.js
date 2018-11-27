660ec8dcce623e9bbd5573f58ce9f05ed105edb5
fauzan_bintang <obin.aji@gmail.com>
2018-11-27 11:16:03 +0700

    fauzan

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
+function checkVowel(word) {
+    const vowels = ['a', 'i', 'u', 'e', 'o']
+    for (let i = 0; i < word.length; i++) {
+        let check = false
+        vowels.forEach(e => {
+            if (word[i] === e) {
+                check = true
+            }
+        });
+        if (check === false) {
+            return false
+        }
+    }
+    return true
+}
+
+console.log(checkVowel('aiu'));
+console.log(checkVowel('aiueo'));
+console.log(checkVowel('aikau'));
 
